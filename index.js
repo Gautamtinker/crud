db9503debce0df0802eb8f515077a1140d38215f
Gautamtinker <gautamtinker83@gmail.com>
2023-12-19 14:27:47 +0530

    item crud

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect(
+  "mongodb+srv://gautamtinker83:qR5TF2Be5HXJuyuF@cluster0.4ibppx7.mongodb.net/",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+// Define a mongoose model
+const Item = mongoose.model("Item", {
+  name: String,
+  description: String,
+});
+
+// CRUD operations
+
+// Create
+app.post("/items", async (req, res) => {
+  try {
+    const newItem = new Item(req.body);
+    await newItem.save();
+    res.status(201).json(newItem);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Read
+app.get("/item", async (req, res) => {
+  try {
+    const items = await Item.find();
+    res.json(items);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Update
+app.put("/items/:id", async (req, res) => {
+  try {
+    const updatedItem = await Item.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.json(updatedItem);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Delete
+app.delete("/item/:id", async (req, res) => {
+  try {
+    await Item.findByIdAndDelete(req.params.id);
+    res.json({ message: "Item deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
